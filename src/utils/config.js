77b77cbdabec35a9e40195075f4de87c8037185b
Akshay_prmar <ar641992@gmail.com>
2018-05-30 17:25:37 +0530

    config related changes

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,65 +1,21 @@
 export const configurationFile = {
     production: {
-      DOMAIN_NAME: "https://snaphunt.com",
-      API_DOMAIN_NAME: "https://api.snaphunt.com",
-      bucketNameForCv: "snaphunt-media-prod",
-      bucketNameForImage: "snaphunt-media-prod",
-      bucketNameForVideo: "snaphunt-media-prod",
-      amazonServer_URL: "//s3.ap-southeast-1.amazonaws.com/",
-      awsS3ForCV: {
-        accessKeyId: "AKIAJJJ3NY4XCSAOFAGQ",
-        secretAccessKey: "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",
-        region: "ap-southeast-1"
-      },
-      awsS3ForMedia: {
-        accessKeyId: "AKIAJJJ3NY4XCSAOFAGQ",
-        secretAccessKey: "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",
-        region: "ap-southeast-1"
-      }
+      DOMAIN_NAME: "https://xyz.com",
+      API_DOMAIN_NAME: "https://api.xyz.com"      
     },
     staging: {
-      DOMAIN_NAME: "https://beta.snaphunt.com",
-      API_DOMAIN_NAME: "https://alpha.snaphunt.com",
-      bucketNameForCv: "snaphunt-media-dev",
-      bucketNameForImage: "snaphunt-media-dev",
-      bucketNameForVideo: "snaphunt-media-dev",
-      amazonServer_URL: "//s3.ap-southeast-1.amazonaws.com/",
-      awsS3ForCV: {
-        accessKeyId: "AKIAJJJ3NY4XCSAOFAGQ",
-        secretAccessKey: "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",
-        region: "ap-southeast-1"
-      },
-      awsS3ForMedia: {
-        accessKeyId: "AKIAJJJ3NY4XCSAOFAGQ",
-        secretAccessKey: "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",
-        region: "ap-southeast-1"
-      }
+      DOMAIN_NAME: "https://beta.xyz.com",
+      API_DOMAIN_NAME: "https://alpha.xyz.com"
+     
     },
     development: {
       DOMAIN_NAME: "http://localhost:3001",
-      API_DOMAIN_NAME: "http://localhost:3000",
-      bucketNameForCv: "snaphunt-media-dev",
-      bucketNameForImage: "snaphunt-media-dev",
-      bucketNameForVideo: "snaphunt-media-dev",
-      amazonServer_URL: "//s3.ap-southeast-1.amazonaws.com/",
-      awsS3ForCV: {
-        accessKeyId: "AKIAJJJ3NY4XCSAOFAGQ",
-        secretAccessKey: "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",
-        region: "ap-southeast-1"
-      },
-      awsS3ForMedia: {
-        accessKeyId: "AKIAJJJ3NY4XCSAOFAGQ",
-        secretAccessKey: "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",
-        region: "ap-southeast-1"
-      }
+      API_DOMAIN_NAME: "http://localhost:3000"
+      
     },
     test: {
       DOMAIN_NAME: "http://localhost:3001",
-      API_DOMAIN_NAME: "http://localhost:3000",
-      bucketNameForCv: "snaphunt-media-dev",
-      bucketNameForImage: "snaphunt-media-dev",
-      bucketNameForVideo: "snaphunt-media-dev",
-      amazonServer_URL: "//s3.ap-southeast-1.amazonaws.com/"
+      API_DOMAIN_NAME: "http://localhost:3000"     
     }
   };
   
